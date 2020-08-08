3fcec5b69b3ea0ef3a619de60ce41331b2ee2c4c
Samuel Olaegbe <olaegbesamuel@gmail.com>
2020-08-09 00:45:23 +0100

    fix:revert directory splitting, compartmentalize js code

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,13 @@
 require('./bootstrap');
+
+import router from './routes';
+import store from './store/index';
+import App from './views/layouts/App';
+import Auth from './views/layouts/Auth';
+
+const app = new Vue({
+    el: '#app',
+    router,
+    store,
+    components: { App, Auth }
+});
--- webpack.mix.js
+++ webpack.mix.js
@@ -11,7 +11,5 @@ const mix = require('laravel-mix');
  |
  */
 
-mix
-    .js('resources/js/onboarding/app.js', 'public/js/onboarding')
-    .js('resources/js/dashboard/app.js', 'public/js/dashboard')
+mix.js('resources/js/app.js', 'public/js')
     .sass('resources/sass/app.scss', 'public/css');
