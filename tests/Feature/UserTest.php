887284ca65e7a533af545c6a741e97093e164732
Onifs10 <onifadebolu64@gmail.com>
2020-08-14 14:55:11 +0100

    start UserCURDv2 with test

---
+++ tests/Feature/UserTest.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\OrganizationUser;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use phpseclib\Crypt\Random;
+use Tests\TestCase;
+
+class UserTest extends TestCase
+{
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function testOrganizationUserLogin()
+    {        $user = OrganizationUser::inRandomOrder()->first(); // pick a user from the database
+        $response = $this->json('post',
+            '/api/OrganizationUser/login',
+            ['orgId' => $user->organization_id,
+                'userId' => $user->user_id,
+                'password' => 'password'
+                ]
+        ); // send a post request to th api with the required data;
+        $response->dump(); //dump response
+        $data = $response; //dump response
+        $token = $data['token'];
+        $response->assertStatus(200);
+        $response->assertJson(['OrganizationUser'=> ['id'=>$user->id]]); //check if the json returned has the user id
+        $this->json('post','/api/OrganizationUser/logout',[],['Authorization' => 'Bearer ' . $token]);
+
+    }
+    public function testChangeUserDisplayName()
+    {
+        $DummyUser = factory(OrganizationUser::class)->make();
+        $user = OrganizationUser::inRandomOrder()->first(); // pick a user from the database
+        $response = $this->json('post',
+            '/api/OrganizationUser/login',
+            ['orgId' => $user->organization_id,
+                'userId' => $user->user_id,
+                'password' => 'password'
+            ]
+        ); // send a post request to th api with the required data
+        $data = $response; //dump response
+        $token = $data['token'];
+        dump($token);
+        dump($data['OrganizationUser']['displayName']);
+        $response->assertStatus(200); // check if successful
+        $response->assertJson(['OrganizationUser'=> ['id'=>$user->id]]); //check if the json returned has the user id
+        $response2 = $this->json(
+                'post',
+                'api/OrganizationUser/changeDisplayName',
+                ['displayName'=> $DummyUser->displayName],
+                ['Authorization' => 'Bearer ' . $token]
+            );
+        $response2->dump(); /// dump response
+        $response2->assertStatus(200);
+        $response3 = $this->json(
+            'get',
+            'api/OrganizationUser/index',
+            [],
+            ['Authorization' => 'Bearer ' . $token]
+        ); // get user details
+         $name = $response3['OrganizationUser']['displayName'];//display new name
+        dump('new name');
+        dump($name);
+        $this->assertNotEquals($response['OrganizationUser']['displayName'],$response3['OrganizationUser']['displayName']);
+        //verify name change
+
+        //logout
+        $response2 = $this->json('post','/api/OrganizationUser/logout',[],['Authorization' => 'Bearer ' . $token]); //not working on unit test but works in postMan
+    }
+
+}
