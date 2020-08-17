eb7747e134bc1d725e6194c842595f873186a325
Samuel Olaegbe <olaegbesamuel@gmail.com>
2020-08-17 17:24:48 +0100

    feat:add authenticated user banner

--- resources/js/components.js
+++ resources/js/components.js
@@ -1,1 +1,2 @@
 Vue.component('sidebar', require('./components/Sidebar').default)
+Vue.component('auth-user-banner', require('./components/AuthUserBanner').default)
---
+++ resources/js/components/AuthUserBanner.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="content__header__section1">
+        <div class="content__header__section1__desc">
+            <a href="/">
+                <img src="{{ asset('../img/primary_logo.png') }}" />
+            </a>
+            <h2>Welcome Jonathan</h2>
+            <p>Your job, your idea</p>
+        </div>
+        <div class="content__header__section1__profile">
+            <i class="far fa-user"></i>
+        </div>
+    </div>
+</template>
