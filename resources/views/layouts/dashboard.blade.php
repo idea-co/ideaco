38a8d3554b53a875bb35c757c8690873937b6c12
Nwokochahannah <hannahsopuruchi@gmail.com>
2020-08-24 08:09:07 +0100

    feat: implemented explore page

--- resources/views/layouts/dashboard.blade.php
+++ resources/views/layouts/dashboard.blade.php
@@ -18,16 +18,7 @@
         <sidebar></sidebar>
         
         <div class="content">
-            <header class="content__header">
-                <auth-user-banner></auth-user-banner>
-                <div class="content__header__section2">
-                    <h2>Explore</h2>
-                    <a class="content__header__section2__link content__header__section2__link--active">Most Active</a>
-                    <a class="content__header__section2__link">Highest Voted</a>
-                    <a class="content__header__section2__link">Most Recent</a>
-                    <a class="content__header__section2__link filter"><i class="fas fa-filter"></i><span> Filter </span><i class="fas fa-caret-down"></i></a>
-                </div>
-            </header>
+            
             @yield('content')
         </div>
 
